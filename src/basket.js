d2e39b083eacd1541ba0665d057fd8fa81e6ea07
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-09 12:32:50 +0100

    good

--- src/basket.js
+++ src/basket.js
@@ -1,49 +1,49 @@
-const Bagel = require("../src/bagel.js");
-const deals = require("../src/deals.js");
+const Bagel = require('../src/bagel.js');
+const deals = require('../src/deals.js');
 
 class Basket {
-    constructor(number = 3) {
-        this.contents = []
-        this.IDcounter = 0
-        this.capacity = number
-        this.counts = {}
-    }
+  constructor(number = 3) {
+    this.contents = [];
+    this.IDcounter = 0;
+    this.capacity = number;
+    this.counts = {};
+  }
 
-    //good
+  //good
 
-    addBagel(SKU, numOfBagels = 1) {      
-        for (let i = 0; i < numOfBagels; i++) {
-            if (!this.basketIsFull()) {
-            this.IDcounter++
-            const id = this.IDcounter
-            let bagelItem = new Bagel(SKU, id)
-            this.contents.push(bagelItem)
-            }
-        }
-        return this.contents
+  addBagel(SKU, numOfBagels = 1) {
+    for (let i = 0; i < numOfBagels; i++) {
+      if (!this.basketIsFull()) {
+        this.IDcounter++;
+        const id = this.IDcounter;
+        let bagelItem = new Bagel(SKU, id);
+        this.contents.push(bagelItem);
+      }
+    }
+    return this.contents;
+  }
+  removeBagel(id) {
+    for (let i = 0; i < this.contents.length; i++) {
+      if (this.contents[i].id === id) {
+        this.contents.splice([i], 1);
+        return this.contents;
+      }
+    }
+    return "Bagel isn't in basket";
+  }
 
+  basketIsFull() {
+    if (this.contents.length >= this.capacity) {
+      return 'basket is full';
     }
-    removeBagel(id) {
-        for (let i = 0; i < this.contents.length; i++) {
-            if (this.contents[i].id === id) {
-                this.contents.splice([i], 1)
-                return this.contents
-            }
-        } return "Bagel isn't in basket"
-    }
+    return false;
+  }
 
-    basketIsFull() {
-        if (this.contents.length >= this.capacity) {
-           return 'basket is full'
-        }
-        return false
-    }
-
-    getPriceOfBagel(SKU) {
-        const output = new Bagel(SKU);
-        return output.price
-    }
-/*
+  getPriceOfBagel(SKU) {
+    const output = new Bagel(SKU);
+    return output.price;
+  }
+  /*
     getTotal() {
         let total = 0
         this.checkDeals()
@@ -54,71 +54,67 @@ class Basket {
      return total/100
     }
 */
-    countBagelsInBasket(){
-        this.counts = {}
-        for (let i = 0; i < this.contents.length; i++){
-            const SKU = this.contents[i]['SKU']
-            if (!this.counts.hasOwnProperty(SKU)) {
-                this.counts[`${SKU}`] = 1
-            } else {
-                this.counts[`${SKU}`]++;
-            }
-        }
-         return this.counts;
+  countBagelsInBasket() {
+    this.counts = {};
+    for (let i = 0; i < this.contents.length; i++) {
+      const SKU = this.contents[i]['SKU'];
+      if (!this.counts.hasOwnProperty(SKU)) {
+        this.counts[`${SKU}`] = 1;
+      } else {
+        this.counts[`${SKU}`]++;
+      }
     }
+    return this.counts;
+  }
 
-    static getSubtotal(counts,SKU){
-        const count = counts[SKU]
-        const dealQuantity = deals[SKU][0]
-        const dealPrice = deals[SKU][1]
-        const bagelPrice = Bagel.getPriceOfBagel(SKU)
-        const dealSum = Math.floor(count / dealQuantity) * (dealPrice)
-        const nonDealSum = (count % dealQuantity) * (bagelPrice)
-        return Number((dealSum + nonDealSum).toFixed(2))
+  static getSubtotal(counts, SKU) {
+    const count = counts[SKU];
+    const dealQuantity = deals[SKU][0];
+    const dealPrice = deals[SKU][1];
+    const bagelPrice = Bagel.getPriceOfBagel(SKU);
+    const dealSum = Math.floor(count / dealQuantity) * dealPrice;
+    const nonDealSum = (count % dealQuantity) * bagelPrice;
+    return Number((dealSum + nonDealSum).toFixed(2));
+  }
 
+  getTotal() {
+    const counts = this.counts;
+    let total = 0;
+    for (let SKU in counts) {
+      const count = counts[`${SKU}`];
+      const dealQuantity = deals[SKU][0];
+      const dealPrice = deals[SKU][1];
+      const bagelPrice = Bagel.getPriceOfBagel(SKU);
+      if (deals.hasOwnProperty(SKU)) {
+        const dealSum = Math.floor(count / dealQuantity) * dealPrice;
+        const nonDealSum = (count % dealQuantity) * bagelPrice;
+        total += dealSum + nonDealSum;
+      }
+      if (dealQuantity === 1) {
+        // adhoc application of coffee deal saving
+        const BOGOFSKU = `${deals[SKU][2]}`;
+        const numOfDiscounts = counts[BOGOFSKU] % deals[BOGOFSKU][0];
+        const saving = Bagel.getPriceOfBagel(BOGOFSKU) - deals[SKU][3];
+        total -= numOfDiscounts * saving;
+      }
     }
+    return Number(total.toFixed(2));
+  }
 
-    getTotal(){
-        const counts = this.counts
-        let total = 0
-        for (let SKU in counts){
-            const count = counts[`${SKU}`]
-            const dealQuantity = deals[SKU][0]
-            const dealPrice = deals[SKU][1]
-            const bagelPrice = Bagel.getPriceOfBagel(SKU)
-            if (deals.hasOwnProperty(SKU)){
-                const dealSum = Math.floor(count / dealQuantity) * (dealPrice)
-                const nonDealSum = (count % dealQuantity) * (bagelPrice)
-                total += dealSum + nonDealSum
-            }
-            if (dealQuantity === 1){                                                 // adhoc application of coffee deal saving
-                const BOGOFSKU = `${deals[SKU][2]}`
-                const numOfDiscounts = counts[BOGOFSKU] % deals[BOGOFSKU][0]
-                const saving = Bagel.getPriceOfBagel(BOGOFSKU) - deals[SKU][3]
-                total -= numOfDiscounts * saving
-            }
-        }
-        return Number(total.toFixed(2))
-    }
-        
-        
-        
-        /*this.contents.filter()
+  /*this.contents.filter()
         for(let i = 0; i < this.contents.length; i++){
             for (let j = 0; j < )
         }
     }
     */
-
 }
 
-const basket = new Basket()
+const basket = new Basket();
 
 // const expected = [new Bagel("BGLO", 1)]
 
-console.log(basket.addBagel("BGLP", 30))
+console.log(basket.addBagel('BGLP', 30));
 
 // console.log(basket.contents)
 
-
-module.exports = Basket
+module.exports = Basket;
--- src/receipt.js
+++ src/receipt.js
@@ -1,50 +1,47 @@
-const Bagel = require("../src/bagel.js");
-const Basket = require("../src/basket.js");
+const Bagel = require('../src/bagel.js');
+const Basket = require('../src/basket.js');
 
 class Receipt {
-    constructor(obj = {}){
-    this.purchases = obj
-    this.date = new Date
-    this.total = 0
-    }
-    getReceipt(){
-        return `
+  constructor(obj) {
+    this.purchases = obj;
+    this.date = new Date();
+    this.total = 0;
+  }
+  getReceipt() {
+    return `
     ~~~ Bob's Bagels ~~~    
 
        ${this.date.toDateString()}
 ----------------------------
 ${this.getPurchaseList()}
-Total                 £${Number(this.total.toFixed(2))}
+Total                 £${+this.total.toFixed(2)}
         Thank you
-      for your order!         `
-    }
-    getPurchaseList(){
-        this.total = 0
-        let purchaseLines = ""
-        for (let key in this.purchases){
-            let receiptLine = ""
-            receiptLine += Bagel.getTypeOfBagel(key)
-            ? Bagel.getTypeOfBagel(key)
-            : 'Coffee'
-            for (let i = 0;i<19;i++){
-                if (receiptLine.length < 19){
-                    receiptLine += " "
-                }
-            }
-            receiptLine += this.purchases[`${key}`]
-            for (let i=0;i<4;i++){
-                if (receiptLine.length < 23){
-                    receiptLine += " "
-                }
-            }
-            receiptLine += "£"
-            const subtotal = Basket.getSubtotal(this.purchases, key)
-            receiptLine += subtotal
-            this.total += subtotal
-            purchaseLines += `${receiptLine}\n`
+      for your order!         `;
+  }
+  getPurchaseList() {
+    this.total = 0;
+    let purchaseLines = '';
+    for (let key in this.purchases) {
+      let receiptLine = '';
+      receiptLine += Bagel.getTypeOfBagel(key) ? Bagel.getTypeOfBagel(key) : 'Coffee';
+      for (let i = 0; i < 19; i++) {
+        if (receiptLine.length < 19) {
+          receiptLine += ' ';
         }
-        return purchaseLines
+      }
+      receiptLine += this.purchases[`${key}`];
+      for (let i = 0; i < 4; i++) {
+        if (receiptLine.length < 23) {
+          receiptLine += ' ';
+        }
+      }
+      receiptLine += '£';
+      const subtotal = Basket.getSubtotal(this.purchases, key);
+      receiptLine += subtotal;
+      this.total += subtotal;
+      purchaseLines += `${receiptLine}\n`;
     }
-
+    return purchaseLines;
+  }
 }
-module.exports = Receipt
+module.exports = Receipt;
